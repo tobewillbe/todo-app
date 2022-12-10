c089be6b5e69a8fb50b824bff20e4a2641b2dd47
wbtb <jhs0580@naver.com>
2022-12-10 14:55:44 +0900

    #3 AddTodoComponent Complete

--- src/App.js
+++ src/App.js
@@ -1,38 +1,56 @@
 //import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 import Todo from './components/Todo';
 import {List} from '@mui/material';
 import AddTodo from "./components/AddTodo";
 
-const items = [
-    {
-        id: 1,
-        title: '점심메뉴 고르기',
-        done: true
-    },
-    {
-        id: 2,
-        title: '책 읽기',
-        done: false
-    },
-    {
-        id: 3,
-        title: '동영상 강의 보기',
-        done: false
-    }
-];
-
-const todoItems = items.map(item => <Todo key={item.id} item={item} />);
 
 const App = () => {
-  return (
+
+    const items = [
+        {
+            id: 1,
+            title: '점심메뉴 고르기',
+            done: true
+        },
+        {
+            id: 2,
+            title: '책 읽기',
+            done: false
+        },
+        {
+            id: 3,
+            title: '동영상 강의 보기',
+            done: false
+        }
+    ];
+
+    //AddTodo에게 전달할 함수
+    //할일 추가처리
+    const add = (item) =>{
+        item.id = itemList.length +1;
+        item.done = false;
+        // console.log('add 호출됨');
+        // console.log(item);
+
+        setItemList(itemList => itemList.concat(item));
+        console.log(itemList);
+    };
+
+    const [itemList, setItemList] =useState(items);
+
+    const todoItems = itemList.map(item => <Todo key={item.id} item={item} />);
+
+    return (
     <div className="App">
-        <AddTodo />
+        <AddTodo add={add}/>
         <List>
             {todoItems}
         </List>
     </div>
   );
+
 }
 
 export default App;
--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,19 +1,47 @@
 import React, {useState} from 'react';
 import {TextField, Paper, Button, Grid} from "@mui/material";
 
-function AddTodo() {
+function AddTodo({add}) {
 
     // 사용자의 입력을 저장할 객체
     const [item, setItem] = useState({ title : '' })
+
+    // 입력창에 값이 변할때 마다 item객체안에 title값 자동 저장
+
+    const changeHandler = e => {
+        // console.log(e.target.value);
+        setItem({title: e.target.value });
+    };
+
+    //입력버튼 클릭 이벤트 핸들러
+    const addClickHandler = e =>{
+        // console.log('버튼클릭');
+        add(item);
+        setItem({title: ' ' });
+    };
+
+    //엔터 이벤트
+    const enterHandler = e =>{
+        if (e.key === 'Enter'){
+            add(item);
+            setItem({title: '' });
+        }
+    }
 
     return (
         <Paper style={{ margin: 16, padding: 16 }}>
             <Grid container>
                 <Grid xs={11} md={11} item style={{paddingRight: 16}}>
-                    <TextField placeholder="Add Todo Here" fullWidth />
+                    <TextField
+                        placeholder="Add Todo Here"
+                        fullWidth
+                        onChange={changeHandler}
+                        value={item.title}
+                        onKeyUp={enterHandler}
+                    />
                 </Grid>
                 <Grid xs={1} md={1} item>
-                    <Button fullWidth color="secondary" variant="outlined">
+                    <Button fullWidth color="secondary" variant="outlined" onClick={addClickHandler}>
                         +
                     </Button>
                 </Grid>
